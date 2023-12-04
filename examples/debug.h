19dffa8c1b841cf97db051a63bc560cdaa156d8f
Tommy Gilligan <7865781+tommy-gilligan@users.noreply.github.com>
2023-12-05 01:04:15 +1100

    Use unsigned types to collect pio state

--- examples/debug.h
+++ examples/debug.h
@@ -1,23 +1,23 @@
 #include "json-maker/json-maker.h"
 
 struct pio_state_copy {
-    int ctrl;
-    int fstat;
-    int fdebug;
-    int flevel;
-    int irq;
-    int dbg_padout;
-    int dbg_padoe;
-    int dbg_cfginfo;
+    unsigned int ctrl;
+    unsigned int fstat;
+    unsigned int fdebug;
+    unsigned int flevel;
+    unsigned int irq;
+    unsigned int dbg_padout;
+    unsigned int dbg_padoe;
+    unsigned int dbg_cfginfo;
 };
 
 struct sm_state_copy {
-    int sm_clkdiv;
-    int sm_execctrl;
-    int sm_shiftctrl;
-    int sm_addr;
-    int sm_instr;
-    int sm_pinctrl;
+    unsigned int sm_clkdiv;
+    unsigned int sm_execctrl;
+    unsigned int sm_shiftctrl;
+    unsigned int sm_addr;
+    unsigned int sm_instr;
+    unsigned int sm_pinctrl;
 };
 
 #define SM0_BASE 0x502000c8
@@ -32,12 +32,12 @@ void print_sm_state(int base) {
     char* p = buff;
     p = json_objOpen( p, NULL, &len );
 
-    p = json_int( p, "sm_clkdiv", *((volatile int *) base), &len );
-    p = json_int( p, "sm_execctrl", *((volatile int *) (base + 4)), &len );
-    p = json_int( p, "sm_shiftctrl", *((volatile int *) (base + 8)), &len );
-    p = json_int( p, "sm_addr", *((volatile int *) (base + 12)), &len );
-    p = json_int( p, "sm_instr", *((volatile int *) (base + 16)), &len );
-    p = json_int( p, "sm_pinctrl", *((volatile int *) (base + 20)), &len );
+    p = json_int( p, "sm_clkdiv", *((volatile unsigned int *) base), &len );
+    p = json_int( p, "sm_execctrl", *((volatile unsigned int *) (base + 4)), &len );
+    p = json_int( p, "sm_shiftctrl", *((volatile unsigned int *) (base + 8)), &len );
+    p = json_int( p, "sm_addr", *((volatile unsigned int *) (base + 12)), &len );
+    p = json_int( p, "sm_instr", *((volatile unsigned int *) (base + 16)), &len );
+    p = json_int( p, "sm_pinctrl", *((volatile unsigned int *) (base + 20)), &len );
 
     p = json_objClose( p, &len );
     p = json_end( p, &len );
@@ -51,14 +51,14 @@ void print_pio_state() {
 
     char* p = buff;
     p = json_objOpen( p, NULL, &len );
-    p = json_int( p, "ctrl", *((volatile int *) 0x50200000), &len );
-    p = json_int( p, "fstat", *((volatile int *) 0x50200004), &len );
-    p = json_int( p, "fdebug", *((volatile int *) 0x50200008), &len );
-    p = json_int( p, "flevel", *((volatile int *) 0x5020000c), &len );
-    p = json_int( p, "irq", *((volatile int *) 0x50200030), &len );
-    p = json_int( p, "dbg_padout", *((volatile int *) 0x5020003c), &len );
-    p = json_int( p, "dbg_padoe", *((volatile int *) 0x50200040), &len );
-    p = json_int( p, "dbg_cfginfo", *((volatile int *) 0x50200044), &len );
+    p = json_int( p, "ctrl", *((volatile unsigned int *) 0x50200000), &len );
+    p = json_int( p, "fstat", *((volatile unsigned int *) 0x50200004), &len );
+    p = json_int( p, "fdebug", *((volatile unsigned int *) 0x50200008), &len );
+    p = json_int( p, "flevel", *((volatile unsigned int *) 0x5020000c), &len );
+    p = json_int( p, "irq", *((volatile unsigned int *) 0x50200030), &len );
+    p = json_int( p, "dbg_padout", *((volatile unsigned int *) 0x5020003c), &len );
+    p = json_int( p, "dbg_padoe", *((volatile unsigned int *) 0x50200040), &len );
+    p = json_int( p, "dbg_cfginfo", *((volatile unsigned int *) 0x50200044), &len );
 
     p = json_objClose( p, &len );
     p = json_end( p, &len );
